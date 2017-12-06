299594014220b6129db6d2821379e05acb8b98c8
Stronach <alex@highlandercom.com>
2017-12-06 08:37:37 -0500

    Created a folder in the project for me to follow along with the tutorials and still write the code myself. I will continue with this as I do more.

---
+++ dataviz/tutorial_source2/parse.py
@@ -0,0 +1,22 @@
+#parse.py
+"""
+  From the original file.
+Data Visualization Project
+
+Parse data from an ugly CSV or Excel file, and render it in
+JSON, save to a database, and visualize in graph form.
+
+Part I: Taking data from a CSV/Excel file, and return it into a format
+that is easier for Python to play with.
+
+Copyright (c) 2013 E. Lynn Root
+Distributed under the zlib png license. See LICENSE for details.
+"""
+import csv
+
+MY_FILE = "../data/sample_sfpd_incident_all.csv"
+
+def parse(raw_file, delimiter):
+    """Parses a raw CSV file to a JSON-line object."""
+    
+    return parsed_data
